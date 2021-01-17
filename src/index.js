e6c22d4b1ea0e874c52bf2d440892a43c9891ade
mikansc <michael.nsc@outlook.com>
2021-01-17 19:53:47 -0300

    refactor: change index structure to handle react router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import HomePage from "./HomePage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import routes from "./routes";
 
 import "./assets/css/global.css";
 
-ReactDOM.render(<HomePage />, document.getElementById("root"));
+const App = () => (
+  <Router basename={process.env.PUBLIC_URL}>
+    <Switch>
+      {routes.map((route) => (
+        <Route
+          key={route.path}
+          path={route.path}
+          component={route.component}
+          exact={route.exact}
+        />
+      ))}
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
