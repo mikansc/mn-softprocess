e3c9bbc4834dae58d676fc8de0a23806d8b0f464
mikansc <michael.nsc@outlook.com>
2021-01-18 03:23:37 -0300

    feat: create ProcessPage and ProcessCard components

---
+++ src/ProcessPage/index.jsx
@@ -0,0 +1,28 @@
+import { Button } from "../components/Button";
+import { Searchbar } from "../components/Searchbar";
+import ProcessCard from "./components/ProcessCard";
+import "./Process.styles.css";
+
+const ProcessPage = () => {
+  return (
+    <div className="process-container">
+      <div className="process-bar">
+        <h1 className="global-title">
+          Busca de <br /> processos
+        </h1>
+        <Searchbar />
+        <Button type="button">NOVO</Button>
+      </div>
+      <div className="process-content">
+        <ProcessCard />
+        <ProcessCard />
+        <ProcessCard />
+        <ProcessCard />
+        <ProcessCard />
+        <ProcessCard />
+      </div>
+    </div>
+  );
+};
+
+export default ProcessPage;
