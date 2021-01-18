5b0bbb183584e1f51a4ce06fbb4aba25f201945b
mikansc <michael.nsc@outlook.com>
2021-01-18 03:29:35 -0300

    feat: create process route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
 import HomePage from "../HomePage";
+import ProcessPage from "../ProcessPage";
 
 const routes = [
   {
     path: "/",
     component: HomePage,
     exact: true,
+  },
+  {
+    path: "/process",
+    component: ProcessPage,
   },
 ];
 
