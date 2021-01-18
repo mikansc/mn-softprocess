ec5106e0bff787543efba176bc4d2d31ab3c6951
mikansc <michael.nsc@outlook.com>
2021-01-18 06:04:58 -0300

    refactor: change some class names and add click handler

--- src/ProcessPage/components/ProcessCard.jsx
+++ src/ProcessPage/components/ProcessCard.jsx
@@ -1,34 +1,40 @@
 import imagePlaceholder from "../../assets/images/placeholder.png";
 import "./ProcessCard.styles.css";
 
-const ProcessCard = () => {
+const ProcessCard = ({ onClick }) => {
+  const handleOnClick = () => {
+    if (onClick) onClick();
+  };
+
   return (
-    <div className="process-card">
+    <div className="process-card" onClick={handleOnClick}>
       <div className="process-card__img">
         <img src={imagePlaceholder} alt="img" />
       </div>
-      <div className="process-card__info">
-        <div className="global-subtitle textcolor-black-54">Número</div>
-        <span>SOFT 0001/2018</span>
-      </div>
-      <div className="process-card__info textcolor-black-54">
-        <div className="global-subtitle">Assunto</div>
-        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
-      </div>
-      <div className="process-card__info textcolor-black-54">
-        <div className="global-subtitle">Interessado</div>
-        <span>SOFT 0001/2018</span>
-      </div>
-      <div className="process-card__info textcolor-black-54">
-        <div className="global-subtitle">Descrição</div>
-        <span>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Reprehenderit deleniti doloribus eveniet ab totam, laborum eos
-          distinctio sequi repellendus recusandae. Ipsam suscipit eaque natus
-          hic adipisci ipsa odio iusto sint corporis labore, harum ex omnis
-          illum, deserunt perferendis, soluta nobis? Quibusdam debitis animi
-          voluptatem ut obcaecati temporibus voluptatum velit aliquam.
-        </span>
+      <div className="process-card__data">
+        <div className="process-card__info">
+          <div className="global-subtitle textcolor-black-54">Número</div>
+          <span>SOFT 0001/2018</span>
+        </div>
+        <div className="process-card__info">
+          <div className="global-subtitle textcolor-black-54">Assunto</div>
+          <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+        </div>
+        <div className="process-card__info">
+          <div className="global-subtitle textcolor-black-54">Interessado</div>
+          <span>SOFT 0001/2018</span>
+        </div>
+        <div className="process-card__info">
+          <div className="global-subtitle textcolor-black-54">Descrição</div>
+          <span>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Reprehenderit deleniti doloribus eveniet ab totam, laborum eos
+            distinctio sequi repellendus recusandae. Ipsam suscipit eaque natus
+            hic adipisci ipsa odio iusto sint corporis labore, harum ex omnis
+            illum, deserunt perferendis, soluta nobis? Quibusdam debitis animi
+            voluptatem ut obcaecati temporibus voluptatum velit aliquam.
+          </span>
+        </div>
       </div>
     </div>
   );
