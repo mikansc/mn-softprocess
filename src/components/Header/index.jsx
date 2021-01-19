92736a501e6733ec0873f9148938e58a7f8bc51e
mikansc <michael.nsc@outlook.com>
2021-01-19 09:39:24 -0300

    refactor: change function names to a clearer ones

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,34 +1,39 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import ProcessNewModal from "../../ProcessNewModal";
 import Button from "../Button";
 import Modal from "../Modal";
 import Searchbar from "../Searchbar";
+import "./Header.styles.css";
 
 const Header = ({ onClickSearchButton }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
-  const buttonClickHandler = () => {
+  const headerNovoButtonHandler = () => {
     setIsOpen(true);
   };
 
-  const searchHandler = () => {
+  const headerSearchHandler = () => {
     if (onClickSearchButton) {
-      console.log("Do the search");
+      onClickSearchButton();
     }
   };
 
   return (
     <>
       <div className="process-bar">
-        <h1 className="global-title">
-          Busca de <br /> processos
-        </h1>
-        <Searchbar onSearch={searchHandler} />
-        <Button onClick={buttonClickHandler} type="button">
+        <Link className="header-link" to="/">
+          <h1 className="global-title">
+            Busca de <br /> processos
+          </h1>
+        </Link>
+        <Searchbar onSearch={headerSearchHandler} />
+        <Button onClick={headerNovoButtonHandler} type="button">
           NOVO
         </Button>
       </div>
       <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-        <p>Aqui vai a página para novo processo</p>
+        <ProcessNewModal />
       </Modal>
     </>
   );
