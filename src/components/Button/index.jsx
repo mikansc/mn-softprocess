a9820c191204263ae80dfa58c653e40225b3edfa
mikansc <michael.nsc@outlook.com>
2021-01-19 14:12:55 -0300

    refactor: add filled property to button styles

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,12 +1,13 @@
 import "./Button.styles.css";
 
-const Button = ({ children, primary = false, ...props }) => {
+const Button = ({ children, primary = false, filled = false, ...props }) => {
   return (
     <button
       {...props}
-      className={`btn global-btn-label textcolor-black-38 ${
-        primary ? "primary" : ""
-      }`}
+      className={`btn global-btn-label textcolor-black-38 
+        ${primary ? "primary" : ""}
+        ${filled ? "filled" : ""}
+      `}
     >
       {children}
     </button>
