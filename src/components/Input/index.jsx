e708a3908efa20154036d8210b39f01009668f85
mikansc <michael.nsc@outlook.com>
2021-01-19 19:53:08 -0300

    feat: create Input component

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,54 @@
+import "./Input.styles.css";
+
+export default function Input(props) {
+  const {
+    type,
+    id,
+    name,
+    placeholder,
+    label,
+    value,
+    onInput,
+    onChange,
+    autofocus,
+    readonly,
+    children,
+    ...rest
+  } = props;
+
+  return (
+    <div className="input-group">
+      <label className="global-subtitle textcolor-black-54" htmlFor={id}>
+        {label}
+      </label>
+      {type === "textarea" ? (
+        <textarea
+          rows="4"
+          id={id}
+          name={name}
+          type="text"
+          placeholder={placeholder}
+          onChange={onChange}
+          autoFocus={autofocus}
+          readOnly={readonly}
+          {...rest}
+        >
+          {value}
+        </textarea>
+      ) : (
+        <input
+          id={id}
+          name={name}
+          type="text"
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          autoFocus={autofocus}
+          readOnly={readonly}
+          {...rest}
+        />
+      )}
+      {children}
+    </div>
+  );
+}
