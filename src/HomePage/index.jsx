3e72cc59b59b80ffea50867ea62b32bccc66842d
mikansc <michael.nsc@outlook.com>
2021-01-19 20:50:10 -0300

    refactor: add Modal

--- src/HomePage/index.jsx
+++ src/HomePage/index.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
+import Modal from "../components/Modal";
 import Searchbar from "../components/Searchbar";
+import ProcessNewModal from "../ProcessNewModal";
 import "./Home.styles.css";
 
 const HomePage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const history = useHistory();
+
+  const headerNovoButtonHandler = () => {
+    setIsModalOpen(true);
+  };
 
   const handleSearch = () => {
     history.push(`/process?q=${searchTerm}`);
   };
 
   return (
-    <div class="home-container">
-      <div className="home-content">
-        <h1 className="home-content__title global-headline textcolor-primary">
-          Busca de processos
-        </h1>
-        <form>
+    <>
+      <div class="home-container">
+        <div className="home-content">
+          <h1 className="home-content__title global-headline textcolor-primary">
+            Busca de processos
+          </h1>
           <div className="search-group">
             <Searchbar
               term={searchTerm}
@@ -25,12 +32,22 @@ const HomePage = () => {
               onSearch={handleSearch}
             />
           </div>
-        </form>
-        <span>
-          Você pode criar um novo processo <a href="!#">clicando aqui</a>
-        </span>
+          <span>
+            Você pode criar um novo processo
+            <button
+              type="button"
+              className="new-process-link"
+              onClick={headerNovoButtonHandler}
+            >
+              clicando aqui
+            </button>
+          </span>
+        </div>
       </div>
-    </div>
+      <Modal open={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <ProcessNewModal />
+      </Modal>
+    </>
   );
 };
 
