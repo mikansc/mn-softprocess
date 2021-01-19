9f38937bb8c14780eab36a7e33b096b4360863e7
mikansc <michael.nsc@outlook.com>
2021-01-19 04:39:23 -0300

    feat: include modal to header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button";
+import Modal from "../Modal";
 import Searchbar from "../Searchbar";
 
-const Header = () => {
+const Header = ({ onClickSearchButton }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const buttonClickHandler = () => {
+    setIsOpen(true);
+  };
+
+  const searchHandler = () => {
+    if (onClickSearchButton) {
+      console.log("Do the search");
+    }
+  };
+
   return (
-    <div className="process-bar">
-      <h1 className="global-title">
-        Busca de <br /> processos
-      </h1>
-      <Searchbar />
-      <Button type="button">NOVO</Button>
-    </div>
+    <>
+      <div className="process-bar">
+        <h1 className="global-title">
+          Busca de <br /> processos
+        </h1>
+        <Searchbar onSearch={searchHandler} />
+        <Button onClick={buttonClickHandler} type="button">
+          NOVO
+        </Button>
+      </div>
+      <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+        <p>Aqui vai a página para novo processo</p>
+      </Modal>
+    </>
   );
 };
 
