382cc2205d21a57f34203c35095ef0d2dda2f5fa
mikansc <michael.nsc@outlook.com>
2021-01-20 20:02:29 -0300

    refactor: change css selectors and classes

--- src/components/Label/index.jsx
+++ src/components/Label/index.jsx
@@ -1,8 +1,8 @@
 import "./Label.styles.css";
 
-const Label = ({ text, children, smallCard = false, selected = false }) => {
+const Label = ({ text, children, smallLabel = false, selected = false }) => {
   return (
-    <div className={`process-card__info ${smallCard ? "shrink" : ""}`}>
+    <div className={`label__info ${smallLabel ? "shrink" : ""}`}>
       <h3
         style={selected ? { color: "var(--color-primary)" } : null}
         className="global-subtitle textcolor-black-54"
