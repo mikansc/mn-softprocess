b4f34c5d83e4323bd72016a731cabacac0841a6c
mikansc <michael.nsc@outlook.com>
2021-01-19 21:13:03 -0300

    refactor: remove searchTerm state from HomePage

--- src/HomePage/index.jsx
+++ src/HomePage/index.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { useHistory } from "react-router-dom";
 import Modal from "../components/Modal";
 import Searchbar from "../components/Searchbar";
 import ProcessNewModal from "../ProcessNewModal";
@@ -7,30 +6,20 @@ import "./Home.styles.css";
 
 const HomePage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
-  const history = useHistory();
 
   const headerNovoButtonHandler = () => {
     setIsModalOpen(true);
   };
 
-  const handleSearch = () => {
-    history.push(`/process?q=${searchTerm}`);
-  };
-
   return (
     <>
-      <div class="home-container">
+      <div className="home-container">
         <div className="home-content">
           <h1 className="home-content__title global-headline textcolor-primary">
             Busca de processos
           </h1>
           <div className="search-group">
-            <Searchbar
-              term={searchTerm}
-              onTextInput={setSearchTerm}
-              onSearch={handleSearch}
-            />
+            <Searchbar />
           </div>
           <span>
             Você pode criar um novo processo
