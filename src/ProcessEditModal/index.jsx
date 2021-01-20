d57b5bf476776676b8310e6ae6f49a79ca1ce630
mikansc <michael.nsc@outlook.com>
2021-01-20 20:56:38 -0300

    refactor: receive props and display data

--- src/ProcessEditModal/index.jsx
+++ src/ProcessEditModal/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Input from "../components/Input";
 import List from "../components/List";
 import Button from "../components/Button";
@@ -6,15 +6,31 @@ import "./ProcessEditModal.styles.css";
 
 export default function ProcessEditModal({ process, onCancel = null }) {
   const [interessado, setInteressado] = useState("");
-  const [interessados, setInteressados] = useState(process.interessados);
+  const [assunto, setAssunto] = useState("");
+  const [interessados, setInteressados] = useState([]);
+  const [descricao, setDescricao] = useState("");
 
-  if (!process) return null;
+  const {
+    assunto: pAssunto,
+    descricao: pDescricao,
+    interessados: pInteressados,
+  } = process;
 
-  const addInteressadoHandler = () => {
+  useEffect(() => {
+    setAssunto(pAssunto);
+    setInteressados(pInteressados);
+    setDescricao(pDescricao);
+  }, [pAssunto, pDescricao, pInteressados]);
+
+  const handleAddInteressado = () => {
     if (interessado !== "") {
       setInteressados([...interessados, interessado]);
       setInteressado("");
     }
+  };
+
+  const handleUpdateProcess = () => {
+    console.log({ id: process.id, assunto, interessados, descricao });
   };
 
   return (
@@ -30,7 +46,8 @@ export default function ProcessEditModal({ process, onCancel = null }) {
               label="Assunto"
               name="assunto"
               id="assunto"
-              value={process.assunto}
+              value={assunto}
+              onChange={setAssunto}
             />
           </div>
         </div>
@@ -45,15 +62,15 @@ export default function ProcessEditModal({ process, onCancel = null }) {
             <Input
               type="text"
               label="Novo interessado"
-              name="listaInteressados"
-              id="listaInteressados"
+              name="interessado"
+              id="interessado"
               value={interessado}
               onChange={(e) => setInteressado(e.target.value)}
               placeholder="Digite o assunto..."
             />
           </div>
           <div className="form-column">
-            <Button type="submit" onClick={addInteressadoHandler}>
+            <Button type="submit" onClick={handleAddInteressado}>
               Adicionar
             </Button>
           </div>
@@ -62,14 +79,18 @@ export default function ProcessEditModal({ process, onCancel = null }) {
         <div className="form-row">
           <Input
             type="textarea"
-            label="Novo interessado"
-            name="listaInteressados"
-            id="assunto"
+            label="Descrição"
+            name="descricao"
+            id="descricao"
             placeholder="Digite a descrição do processo..."
+            value={descricao}
+            onChange={setDescricao}
           />
         </div>
         <div className="form-footer">
-          <Button filled>SALVAR</Button>
+          <Button filled onClick={handleUpdateProcess}>
+            SALVAR
+          </Button>
           <Button onClick={onCancel}>Cancelar</Button>
         </div>
       </div>
