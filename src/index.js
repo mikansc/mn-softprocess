f00709132dea9be72a511d66d3f68e9ef7b0eac5
mikansc <michael.nsc@outlook.com>
2021-01-21 08:05:54 -0300

    feat: implement toastify

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 import routes from "./routes";
 
+import "react-toastify/dist/ReactToastify.css";
 import "./assets/css/global.css";
 
 const App = () => (
-  <Router basename={process.env.PUBLIC_URL}>
-    <Switch>
-      {routes.map((route) => (
-        <Route
-          key={route.path}
-          path={route.path}
-          component={route.component}
-          exact={route.exact}
-        />
-      ))}
-    </Switch>
-  </Router>
+  <>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        {routes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            component={route.component}
+            exact={route.exact}
+          />
+        ))}
+      </Switch>
+    </Router>
+    <ToastContainer autoClose={5000} />
+  </>
 );
 
 ReactDOM.render(<App />, document.getElementById("root"));
