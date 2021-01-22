b79b3bd538de17252e982ff66bedf3938b64fe24
mikansc <michael.nsc@outlook.com>
2021-01-22 14:58:01 -0300

    chore: change services/constants to use env variable

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,4 +1,1 @@
-export const BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3002"
-    : "https://mn-softprocess.heroku.app";
+export const BASE_URL = process.env.REACT_APP_API_BASE_URL;
